570f70c6251bbad7c45d5492feeca40e11096007
AugustoM94 <email@github.com>
2024-01-19 17:59:13 +0100

    added App.vue

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>Ciao</h1>
+    <ul>
+      <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+    </ul>
+    <button @click="nextPage">Avanti</button>
+    <button @click="goToLastPage">Indietro</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { store } from "./store";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      store,
+      projects: [],
+      currentPage: 1,
+      lastPage: 0,
+    };
+  },
+  methods: {
+    getAllProjects() {
+      axios.get(store.apiUrl + "/projects", { params: { page: this.currentPage } })
+        .then((res) => {
+          this.projects = res.data.result.data;
+          this.currentPage = res.data.current_page;
+          this.lastPage = res.data.last_page;
+        })
+        .catch((error) => {
+          console.error("Error fetching projects:", error);
+        });
+    },
+    nextPage() {
+      if (this.currentPage < this.lastPage) {
+        this.currentPage++;
+        this.getAllProjects();
+      }
+    },
+    goToLastPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+        this.getAllProjects();
+      }
+    },
+  },
+  mounted() {
+    this.getAllProjects();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
