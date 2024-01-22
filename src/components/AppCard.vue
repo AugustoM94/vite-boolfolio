79e729e7c9131239d04b56a023f133a47ce2e9b0
AugustoM94 <email@github.com>
2024-01-22 17:50:02 +0100

    create card

---
+++ src/components/AppCard.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="card h-100" style="width: 21rem;">
+        <div class="card-body">
+            <router-link :to="{ name: 'single-project', params: { slug: project.slug } }">
+                <h5 class="card-title">{{ project.title }}</h5>
+            </router-link>
+            <p class="card-text">{{ project.body }}</p>
+            <div class="card-meta">
+                <p class="card-date">{{ project.link }}</p>
+                <span class="card-category">{{ project.category }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { store } from "../store";
+
+export default {
+    name: 'AppCard',
+    props: ['project'],
+    data() {
+        return {
+            store
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" >
+
+.card{
+    background-color: white;
+}
+
+.card-body{
+    color: black;
+}
+
+
+
+</style>
